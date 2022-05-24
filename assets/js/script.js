f41a2a5c167a8764767eaf7b08ee2d05c5ea8e84
trishaalipio <105207789+trishaalipio@users.noreply.github.com>
2022-05-24 22:33:57 +0800

    Fixed mobile and added portfolio section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
     AOS.init();
+
+    $('.portfolio-card').on('click', function(){
+        var $cardNum = $(this).attr('data-number')
+        var $imgSrc = $('.portfolio-modal').eq($cardNum).find('img').attr('data-img');
+
+        $('.modal-container').fadeIn();
+
+        $('.portfolio-modal').eq($cardNum).find('img').attr('src', $imgSrc);
+
+        $('.portfolio-modal').eq($cardNum).find('.portfolio-close').fadeIn();
+
+        console.log($cardNum);
+
+
+    });
+
+    $('.portfolio-close').on('click', function(){
+        $('.modal-container').fadeOut();
+        $(this).prev().attr('src', '');
+        $(this).fadeOut();
+    });
+
+    $('.fullscreen-nav ul a').on('click', function(){
+        $('.fullscreen-nav').fadeOut();
+    });
+
+    $('.burger-wrapper').on('click', function(){
+        $('.fullscreen-nav').fadeIn();
+    });
+
 });
